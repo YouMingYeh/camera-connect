0eb06f9c77e1042627210a59db1a4aa3b2955451
YouMingYeh <b10705052@ntu.edu.tw>
2024-04-23 15:06:27 +0800

    feat: Add camera

--- app/screens/CameraScreen.tsx
+++ app/screens/CameraScreen.tsx
@@ -1,8 +1,14 @@
-import React, { FC } from "react"
+import React, { FC, useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { AppStackScreenProps } from "app/navigators"
 import { Screen, Text } from "app/components"
+import {
+  CameraDevice,
+  useCameraDevice,
+  useCameraDevices,
+  useCameraPermission,
+} from "react-native-vision-camera"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "app/models"
 
@@ -14,6 +20,27 @@ export const CameraScreen: FC<CameraScreenProps> = observer(function CameraScree
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const [device, setDevice] = useState<CameraDevice | null>(null)
+  const cameraDevice = useCameraDevice("back")
+  const { hasPermission, requestPermission } = useCameraPermission()
+
+  useEffect(() => {
+    if (!device && cameraDevice) {
+      setDevice(cameraDevice)
+    }
+
+    if (!hasPermission) {
+      requestPermission()
+    }
+  }, [])
+  if (!device || !hasPermission) {
+    return (
+      <Screen style={$root} preset="scroll">
+        <Text text="no device or permission" />
+      </Screen>
+    )
+  }
+
   return (
     <Screen style={$root} preset="scroll">
       <Text text="camera" />
