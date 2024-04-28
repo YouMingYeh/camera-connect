0835d4ca96b263702cc61cfc1e5b6c75c9bedaee
andre.liang <54308739+namwoam@users.noreply.github.com>
2024-04-28 14:48:55 +0800

    feat: add signout button, need UI design

--- app/screens/ProfileScreen.tsx
+++ app/screens/ProfileScreen.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { Button, ViewStyle, View } from "react-native"
 import { AppStackScreenProps } from "app/navigators"
 import { Screen, Text } from "app/components"
+import { supabase } from "../utils/supabase"
+import { useStores } from "../models"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "app/models"
 
-interface ProfileScreenProps extends AppStackScreenProps<"Profile"> {}
+interface ProfileScreenProps extends AppStackScreenProps<"Profile"> { }
 
 export const ProfileScreen: FC<ProfileScreenProps> = observer(function ProfileScreen() {
   // Pull in one of our MST stores
@@ -14,9 +16,20 @@ export const ProfileScreen: FC<ProfileScreenProps> = observer(function ProfileSc
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const {
+    authenticationStore: { setAuthToken },
+  } = useStores()
+  async function signout() {
+    const { error } = await supabase.auth.signOut()
+    if (error) return
+    setAuthToken("")
+  }
   return (
     <Screen style={$root} preset="scroll">
-      <Text text="profile" />
+      <View>
+        <Text text="profile" />
+        <Button onPress={signout} title="登出"></Button>
+      </View>
     </Screen>
   )
 })
